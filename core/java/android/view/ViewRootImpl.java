Kalpu-24/frameworks_base

Add Alert Slider user interface [SQUASHED]
Ported from OxygenOS and reworked for our alert slider
implementation. We target AudioManager instead of Zen,
icons are also the same as aosp and the dialog uses the
material theme as well as support for our themes.

To use, the alert slider config must be enabled. By default,
the dialog shows on the left side. To move it to the right
side, set the location config to 1.

Squashed:

    From: ZVNexus <zvnexus@outlook.com>
    Date: Fri, 24 Jan 2020 17:00:41 -0500
    Subject: AlertSlider: Make tri-state SystemUI dialog dimensions conditional

    Adjust this based on the left/right config.

    From: Pranav Vashi <neobuddy89@gmail.com>
    Date: Sun, 23 Feb 2020 17:13:34 +0530
    Subject: AlertSlider: Work better with Key Handlers

    * Let Key Handler send intent to let know slider movement.
    * Helps not showing dialog every time ringer changes.

    From: Pranav Vashi <neobuddy89@gmail.com>
    Date: Sun, 23 Feb 2020 23:38:41 +0530
    Subject: AlertSlider: Do not hardcode slider position based on ringer mode

    * Let Key Handler send position details. Different ringer options can be now assigned to different position.
    * Also increase dialog show timeout slightly to avoid race with Key Handler.

    From: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Date: Mon, 24 Feb 2020 22:44:49 +0200
    Subject: AlertSlider: Use default position behaviour if position not specified in intent

    From: Pranav Vashi <neobuddy89@gmail.com>
    Date: Sun, 1 Mar 2020 00:38:13 +0530
    Subject: AlertSlider: Improve layout

    From: Ali B <abittin@gmail.com>
    Date: Sat, 14 Mar 2020 00:19:59 +0300
    Subject: AlertSlider: refactor to reflect slider state

    As there is the possibility of having more option values
    for the alertslider (such as various zen mode states)
    in addition to the current ringer mode states,
    update logic to reflect the value slider's position is
    set to instead of the ringer state we w

core/java/android/view/ViewRootImpl.java
@@ -1710,7 +1710,7 @@ public final class ViewRootImpl implements ViewParent,
                                     + mWindow + " -- another window of type "
                                     + mWindowAttributes.type + " already exists");
                         case WindowManagerGlobal.ADD_PERMISSION_DENIED:
-                            if (com.android.internal.util.evolution.lunaris.shouldBypassTaskPermission(Binder.getCallingUid())) {
+                            if (com.android.internal.util.lunaris.PixelPropsUtils.shouldBypassTaskPermission(Binder.getCallingUid())) {
                                 return;
                             } else {
                                 throw new WindowManager.BadTokenException("Unable to add window "

packages/SystemUI/src/com/android/systemui/power/EnhancedEstimatesImpl.java
@@ -7,7 +7,7 @@ import android.provider.Settings;
 import android.util.KeyValueListParser;
 import android.util.Log;
 
-import com.android.internal.util.evolution.Utils;
+import com.android.internal.util.lunaris.Utils;
 import com.android.settingslib.fuelgauge.Estimate;
 import com.android.settingslib.utils.PowerUtil;
 import com.android.systemui.dagger.SysUISingleton;

packages/SystemUI/src/com/android/systemui/qrcodescanner/controller/QRCodeScannerController.java
@@ -32,7 +32,7 @@ import android.provider.Settings;
 import android.util.Log;
 
 import com.android.internal.config.sysui.SystemUiDeviceConfigFlags;
-import com.android.internal.util.evolution.Utils;
+import com.android.internal.util.lunaris.Utils;
 import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.settings.UserTracker;

services/core/java/com/android/server/display/AutoDimService.java
@@ -18,7 +18,7 @@ package com.android.server.display;
 import static android.provider.Settings.Secure.REDUCE_BRIGHT_COLORS_ACTIVATED;
 import static android.provider.Settings.Secure.EXTRA_DIM_AUTO_MODE;
 import static android.provider.Settings.Secure.EXTRA_DIM_AUTO_TIME;
-import static com.android.internal.util.evolution.AutoSettingConsts.MODE_DISABLED;
+import static com.android.internal.util.lunaris.AutoSettingConsts.MODE_DISABLED;
 
 import android.content.Context;
 import android.hardware.display.ColorDisplayManager;

services/voiceinteraction/java/com/android/server/voiceinteraction/VoiceInteractionManagerService.java
@@ -107,7 +107,7 @@ import com.android.internal.app.IVoiceInteractor;
 import com.android.internal.content.PackageMonitor;
 import com.android.internal.os.BackgroundThread;
 import com.android.internal.util.DumpUtils;
-import com.android.internal.util.evolution.Utils;
+import com.android.internal.util.lunaris.Utils;
 import com.android.server.FgThread;
 import com.android.server.LocalServices;
 import com.android.server.SoundTriggerInternal;

Mist-OS-Staging/frameworks_base_new

core/java/android/view/ViewRootImpl.java
@@ -1710,7 +1710,7 @@ public final class ViewRootImpl implements ViewParent,
                                     + mWindow + " -- another window of type "
                                     + mWindowAttributes.type + " already exists");
                         case WindowManagerGlobal.ADD_PERMISSION_DENIED:
-                            if (com.android.internal.util.evolution.mist.shouldBypassTaskPermission(Binder.getCallingUid())) {
+                            if (com.android.internal.util.mist.PixelPropsUtils.shouldBypassTaskPermission(Binder.getCallingUid())) {
                                 return;
                             } else {
                                 throw new WindowManager.BadTokenException("Unable to add window "

packages/SystemUI/src/com/android/systemui/power/EnhancedEstimatesImpl.java
@@ -7,7 +7,7 @@ import android.provider.Settings;
 import android.util.KeyValueListParser;
 import android.util.Log;
 
-import com.android.internal.util.evolution.Utils;
+import com.android.internal.util.mist.Utils;
 import com.android.settingslib.fuelgauge.Estimate;
 import com.android.settingslib.utils.PowerUtil;
 import com.android.systemui.dagger.SysUISingleton;

packages/SystemUI/src/com/android/systemui/qrcodescanner/controller/QRCodeScannerController.java
@@ -32,7 +32,7 @@ import android.provider.Settings;
 import android.util.Log;
 
 import com.android.internal.config.sysui.SystemUiDeviceConfigFlags;
-import com.android.internal.util.evolution.Utils;
+import com.android.internal.util.mist.Utils;
 import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.dagger.qualifiers.Background;
 import com.android.systemui.settings.UserTracker;

services/core/java/com/android/server/display/AutoDimService.java
@@ -18,7 +18,7 @@ package com.android.server.display;
 import static android.provider.Settings.Secure.REDUCE_BRIGHT_COLORS_ACTIVATED;
 import static android.provider.Settings.Secure.EXTRA_DIM_AUTO_MODE;
 import static android.provider.Settings.Secure.EXTRA_DIM_AUTO_TIME;
-import static com.android.internal.util.evolution.AutoSettingConsts.MODE_DISABLED;
+import static com.android.internal.util.mist.AutoSettingConsts.MODE_DISABLED;
 
 import android.content.Context;
 import android.hardware.display.ColorDisplayManager;

services/voiceinteraction/java/com/android/server/voiceinteraction/VoiceInteractionManagerService.java
@@ -107,7 +107,7 @@ import com.android.internal.app.IVoiceInteractor;
 import com.android.internal.content.PackageMonitor;
 import com.android.internal.os.BackgroundThread;
 import com.android.internal.util.DumpUtils;
-import com.android.internal.util.evolution.Utils;
+import com.android.internal.util.mist.Utils;
 import com.android.server.FgThread;
 import com.android.server.LocalServices;
 import com.android.server.SoundTriggerInternal;