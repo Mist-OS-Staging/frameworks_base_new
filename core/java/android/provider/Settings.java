1961d95ac262d61d407d2f66c25efc2e837e2194
Ghosuto <clash.raja10@gmail.com>
2025-11-11 17:15:21 +0000

    SystemUI: Add Bangabda calendar support [1/2]
    Change-Id: Ib3ce0d87404c0d83ae7faa51d6078cd568a85a98
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7130,6 +7130,7 @@ public final class Settings {
          * 0 = Default
          * 1 = Lunar (Chinese)
          * 2 = Saka (Indian)
+         * 3 = Bangabda (Indian)
          * @hide
          */
         public static final String QS_CALENDAR_TYPE = "qs_calendar_type";
